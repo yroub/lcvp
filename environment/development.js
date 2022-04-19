6d026a6d2c20290b98eead3601052c7259ef02dd
Yannick Roubieu <yannickroubieu@gmail.com>
2022-04-19 16:48:53 +0200

    ready for next chapter

--- environment/development.js
+++ environment/development.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
   dbUrl: 'mongodb+srv://dev:leon@cluster0.ez55o.mongodb.net/lcvp?retryWrites=true&w=majority',
-  cert: path.join( __dirname, '../ssl/localhost.crt'),
-  key: path.join( __dirname, '../ssl/localhost.key'),
+  cert: path.join( __dirname, '../ssl/local.crt'),
+  key: path.join( __dirname, '../ssl/local.key'),
   portHttp: 3000,
   portHttps: 3001
 }
